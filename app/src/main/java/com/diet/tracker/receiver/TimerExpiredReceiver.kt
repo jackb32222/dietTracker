730e2d518573cd187c999db82770be41b46c4e29
dungnt2 <dungnt2@coccoc.com>
2021-08-31 21:19:14 +0700

    sound when showing notification

--- app/src/main/java/com/diet/tracker/receiver/TimerExpiredReceiver.kt
+++ app/src/main/java/com/diet/tracker/receiver/TimerExpiredReceiver.kt
@@ -3,10 +3,12 @@ package com.diet.tracker.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.media.RingtoneManager
+import android.net.Uri
 import com.diet.tracker.notification.DietNotificationManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class TimerExpiredReceiver : BroadcastReceiver() {
@@ -15,9 +17,14 @@ class TimerExpiredReceiver : BroadcastReceiver() {
         const val ACTION_TIMER_EXPIRED = "com.diet.tracker.TIMER_EXPIRED"
     }
 
-    @Inject lateinit var notificationManager: DietNotificationManager
+    @Inject
+    lateinit var notificationManager: DietNotificationManager
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
         notificationManager.showAlarmNotification()
+
+        val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val r = RingtoneManager.getRingtone(context.applicationContext, notification)
+        r.play()
     }
 }
